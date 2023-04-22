5abf5c1b5100b70d5ab625fcc587e7f868ccfdba
Vadzimdi <vadzimdi@gmail.com>
2023-04-22 23:19:45 +0300

    added base settings for layout

--- web/views.py
+++ web/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 
 def index(request):
     return render(request, "web/index.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+
+        # Check if Successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, "web/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "web/login.html")    
+
+
 
