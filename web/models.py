d80040e9d4b2844cfccf14d9712249a1cf4a3505
Vadzimdi <vadzimdi@gmail.com>
2023-04-23 21:19:42 +0300

    added models Comments and Post

--- web/models.py
+++ web/models.py
@@ -3,4 +3,24 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')
+    content = models.CharField(max_length=140)
+    creation_date = models.DateTimeField(auto_now_add=True, blank=True)
+    number_of_likes = models.IntegerField(default=0)
+    comments = models.ManyToManyField('Comments', blank=True, related_name="comments")
+
+    def __str__(self):
+        return f'{self.user} write a post {self.content}'
+    
+
+class Comments(models.Model):
+    text = models.CharField(max_length=255)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True, related_name="user_comment")
+
+
+    
--- web/views.py
+++ web/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from django.db import IntegrityError
+
+from .models import *
 
 
 def index(request):
@@ -25,6 +28,41 @@ def login_view(request):
             })
     else:
         return render(request, "web/login.html")    
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Check if passwords are same
+        if password != confirm_password:
+            return render(request, "web/register.html", {
+                "message": "Passwords must match."
+            })
+        
+        else:
+            # Create a new user
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+            except IntegrityError:
+                return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, "web/register.html")
+
+
 
 
 
